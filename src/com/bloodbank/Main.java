e9110620c5d2b08a3a1f6d952c1eaa3448d15e85
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2025-10-27 22:47:27 +0530

    Add Main class to launch LoginForm

---
+++ src/com/bloodbank/Main.java
@@ -0,0 +1,13 @@
+package com.bloodbank;
+import com.bloodbank.view.LoginForm; 
+import javax.swing.SwingUtilities;
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LoginForm().setVisible(true);
+            }
+        });
+    }
+}
